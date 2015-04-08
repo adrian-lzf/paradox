6c2b35855a42bb8b237093b68d6d970959a5d9c2
adrian-lzf <396273451@qq.com>
2015-04-08 18:08:01 +0800

    Update NumGuess

--- NumGuess.cpp
+++ NumGuess.cpp
@@ -8,8 +8,8 @@ int main()
 	int num1, num2, num3, ges1, ges2, ges3, A, B, i, win = 0, lose = 0;
     char stt; //determine whether to start
     
-    while (stt =! 'Y' && stt =!'y' && stt =! 'N' && stt =!'n')
-    {cout << "éœ€è¦ç©æ¸¸æˆå—ï¼ˆY/Nï¼‰ï¼Ÿ";
+    while (stt != 'Y' && stt != 'y' && stt != 'N' && stt != 'n')
+    {cout << "ĞèÒªÍæÓÎÏ·Âğ£¨Y/N£©£¿";
 	cin >> stt;}
     while ((stt == 'Y') or (stt == 'y')) //let the game begin again if the user wants to
     {
@@ -19,7 +19,7 @@ int main()
     	while (num3 == num2 or num3 == num1) num3 = rand()%9+1; //insure that the three numbers are all distinct
     	for(i=0; i<7; ++i){
         	A = B = 0;
-        	cout << "è¯·è¾“å…¥ä½ çŒœæµ‹çš„æ•°å­—: " << endl;
+        	cout << "ÇëÊäÈëÄã²Â²âµÄÊı×Ö: " << endl;
         	cin >> ges1 >> ges2 >> ges3;
         	if (ges1 == num1) A += 1; if (ges2 == num2) A += 1; if (ges3 == num3) A += 1; 
 			//when the right number is in the right position, A += 1 
@@ -27,16 +27,16 @@ int main()
         	if (ges2 == num1 or ges2 == num3) B += 1;
         	if (ges3 == num1 or ges3 == num2) B += 1;
         	//when the right number is in the wrong position, B += 1
-        	if (A == 3){cout << "æ­å–œï¼Œä½ çŒœå¯¹äº†ï¼" << endl; win += 1; break;}
+        	if (A == 3){cout << "¹§Ï²£¬Äã²Â¶ÔÁË£¡" << endl; win += 1; break;}
         	else cout << A << "A" << B << "B" << endl;
-        	if (i == 6){cout << "å¾ˆé—æ†¾ï¼Œä½ æ²¡æœ‰åœ¨è§„å®šçš„æ¬¡æ•°å†…çŒœå¯¹ã€‚æ­£ç¡®çš„ç­”æ¡ˆæ˜¯" << num1 << num2 << num3 << endl; lose += 1;}}
+        	if (i == 6){cout << "ºÜÒÅº¶£¬ÄãÃ»ÓĞÔÚ¹æ¶¨µÄ´ÎÊıÄÚ²Â¶Ô¡£ÕıÈ·µÄ´ğ°¸ÊÇ" << num1 << num2 << num3 << endl; lose += 1;}}
         do
-        {cout << "éœ€è¦ç©æ¸¸æˆå—ï¼ˆY/Nï¼‰ï¼Ÿ";
+        {cout << "ĞèÒªÍæÓÎÏ·Âğ£¨Y/N£©£¿";
 		cin >> stt;} //the loop starts again
-	while (stt =! 'Y' && stt =!'y' && stt =! 'N' && stt =!'n')
+		while (stt != 'Y' && stt != 'y' && stt != 'N' && stt != 'n');
     }
     
-	cout << "ä½ ä¸€å…±ç©äº†" << win + lose << "å±€ï¼Œèµ¢äº†" << win << "å±€ï¼Œè¾“äº†" << lose << "å±€"; //show the results
+	cout << "ÄãÒ»¹²ÍæÁË" << win + lose << "¾Ö£¬Ó®ÁË" << win << "¾Ö£¬ÊäÁË" << lose << "¾Ö"; //show the results
     return 0;
 
 }
